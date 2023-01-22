ea6bfea65aa013a5f8f7951448dca61d491c5027
Kushal Shah <kushalshah@flash.net>
2023-01-21 21:38:32 -0800

    alot done

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,56 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `PersonalWebsite`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Kushal Shah`,
+    siteUrl: `https://www.kushalhshah.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Source Code Pro`,
+            file: `https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;600;700&display=swap`,
+          },
+          {
+            name: `Inter`,
+            file: `https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/svgs/logo.svg',
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /svgs/
+        }
+      }
+    },
+    {
+      resolve: "gatsby-plugin-anchor-links",
+      options: {
+        offset: -100,
+      }
+    }
+  ],
 };
 
 export default config
---
+++ src/components/Main.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+export const Main = () => {
+  return (
+    <div className="h-screen grid place-items-center">
+      <div className="flex-col flex p-8 lg:w-4/6 w-5/6">
+        <span className="font-mono text-t-green md:text-2xl pb-2 text-lg">
+          <Typewriter
+            options={{
+              strings: [
+                "Hello I am",
+                "Hola yo soy",
+                "Bonjour je suis",
+                "Hallo ik ben",
+                "مرحبا أنا",
+                "Ciao io sono",
+                "こんにちは私は",
+                "Hallo ich bin",
+                "Здравствуйте, я",
+              ],
+              autoStart: true,
+              loop: true,
+              // @ts-expect-error
+              pauseFor: 5000,
+            }}
+          />
+        </span>
+        <span className="lg:text-8xl md:text-6xl font-bold md:pb-4 pb-2 text-4xl">Kushal Shah</span>
+        <span className="lg:text-8xl md:text-6xl font-bold pb-4 text-t-green text-4xl">
+          I create <wbr/>
+          <span className="font-mono">{"<code>"}</span>
+        </span>
+        <span className="text-xl w-4/5 md:text-xl text-lg">
+         I'm a developer focussed on building user experiences through design, innovation, and a lot of code. Currently, I am building onboarding solutions for customers at <a href="https://www.stripe.com" className="text-t-green font-semibold hover:underline">Stripe</a>.
+        </span>
+        <a type="button" href="https://drive.google.com/file/d/1e4YQjQhD4VjaS5cZr3T147_ewHleWOxf/view?usp=sharing" className="w-min border-2 border-t-green px-5 py-2.5 text-center rounded-lg mt-5 hover:bg-t-green-hover text-t-green">Resume</a>
+      </div>
+      <div></div>
+    </div>
+  );
+};
